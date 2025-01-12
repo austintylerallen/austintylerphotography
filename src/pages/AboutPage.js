821903a1c93fa33164d47db0ce355a947a836931
austinallen <austintallen07@gmail.com>
2025-01-11 21:17:15 -0700

    made the about page responsive to mobile devices.

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,29 +4,29 @@ import { motion } from "framer-motion";
 const AboutPage = () => {
   return (
     <motion.div
-      initial={{ opacity: 0, x: -50 }}     // Start state
-      animate={{ opacity: 1, x: 0 }}      // End state
-      exit={{ opacity: 0, x: 50 }}        // Exit state
-      transition={{ duration: 0.6 }}      // Animation duration
-      className="p-8 flex items-center justify-center min-h-screen md:ml-64"
+      initial={{ opacity: 0, x: -50 }} // Start state
+      animate={{ opacity: 1, x: 0 }} // End state
+      exit={{ opacity: 0, x: 50 }} // Exit state
+      transition={{ duration: 0.6 }} // Animation duration
+      className="p-8 flex flex-col md:flex-row items-center justify-center min-h-screen md:ml-64"
     >
       {/* Photo Section */}
-      <div className="flex-shrink-0">
+      <div className="flex-shrink-0 mb-8 md:mb-0 md:mr-8">
         <motion.img
           src="/images/austintyler.jpg"
           alt="Austin Tyler"
           className="rounded-lg shadow-lg w-64 h-auto object-cover"
-          initial={{ scale: 0.8, opacity: 0 }}    // Start state
-          animate={{ scale: 1, opacity: 1 }}      // End state
+          initial={{ scale: 0.8, opacity: 0 }} // Start state
+          animate={{ scale: 1, opacity: 1 }} // End state
           transition={{ duration: 0.6, delay: 0.3 }} // Delay for a staggered effect
         />
       </div>
 
       {/* Text Section */}
       <motion.div
-        className="ml-8"
-        initial={{ opacity: 0, x: 50 }}     // Start state
-        animate={{ opacity: 1, x: 0 }}      // End state
+        className="text-center md:text-left"
+        initial={{ opacity: 0, x: 50 }} // Start state
+        animate={{ opacity: 1, x: 0 }} // End state
         transition={{ duration: 0.6, delay: 0.5 }} // Slight delay for staggered effect
       >
         <h1 className="text-4xl font-bold mb-8">About Austin Tyler</h1>
